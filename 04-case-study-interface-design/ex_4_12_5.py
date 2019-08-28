64d73b839db6f950802a5caa1ca52e3a84cbbc37
Alexander Shirapov <cccp2006_06@mail.ru>
2019-08-29 00:53:40 +0800

    Fibonacci spiral. Ex.5 from ch.4.12

--- 04-case-study-interface-design/ex_4_12_5.py
+++ 04-case-study-interface-design/ex_4_12_5.py
@@ -5,7 +5,39 @@ by Allen Downey
 http://thinkpython2.com
 """
 
+import math
+import time
 import turtle
+
+
+def polyline(t, n, length, angle):
+    """Draws n line segments.
+
+    :param t: Turtle object
+    :param n: number of line segments
+    :param length: length of each segments
+    :param angle: degrees between segments
+    """
+
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle
+
+    :param t: Turtle object
+    :param r: radius of the arc
+    :param angle: angle subtended by the arc, in degrees
+    """
+
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 3
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    polyline(t, n, step_length, step_angle)
 
 
 def arch_spiral(t, n, length=4):
@@ -30,10 +62,25 @@ def arch_spiral(t, n, length=4):
         theta += dtheta
 
 
+def fib_spiral(t, n):
+    """Draws a Fibonacсi spiral.
+
+    :param t: Turtle object
+    :param n: length of sequence
+    """
+
+    a, b = 0, 1
+
+    for i in range(n):
+        arc(t, a, 90)
+        a, b = b, a+b
+
+
 if __name__ == '__main__':
     bob = turtle.Turtle()
 
-    arch_spiral(bob, 200)
+    # arch_spiral(bob, 200)
+    fib_spiral(bob, 15)
 
     bob.hideturtle()
     turtle.mainloop()
