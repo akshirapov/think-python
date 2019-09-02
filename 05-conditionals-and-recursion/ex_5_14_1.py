45ce63f066b3df5b49adf85b5e9a11e62249a6ac
Alexander Shirapov <cccp2006_06@mail.ru>
2019-09-02 12:03:21 +0800

    Converting time. Ex.1 from ch.5.14

---
+++ 05-conditionals-and-recursion/ex_5_14_1.py
@@ -0,0 +1,24 @@
+
+"""This module contains a code for ex.1 related to ch.5.14.
+
+Think Python, 2nd Edition
+by Allen Downey
+http://thinkpython2.com
+"""
+
+import time
+
+
+if __name__ == '__main__':
+    """Converts the current time to a time of days, hours, minutes and 
+    seconds from the epoch.
+    """
+
+    now = time.time()
+
+    hours = int(now // (60*60))
+    minutes = int(now // 60 - hours*60)
+    seconds = int(now % 60)
+    days = int(hours // 24)
+
+    print(f'{days} days {hours-24*days} hours {minutes} minutes {seconds} seconds since the epoch.')
