42d80519708f4d8a0e124e0c3af39d6fa3e15470
Alexander Shirapov <cccp2006_06@mail.ru>
2019-12-16 17:36:05 +0800

    Markov analysis. Ex.8 from ch.13.1

---
+++ 13-case-study-data-structure-selection/ex_13_1_8.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module contains a code for ex.8 related to ch.13.1 of
+Think Python, 2nd Edition
+by Allen Downey
+http://thinkpython2.com
+"""
+
+
+import string
+import random
+
+from ex_13_1_2 import skip_header
+from pprint import pprint
+
+
+def process_word(word: str):
+    """Strips whitespace and punctuation from the word."""
+
+    chars = string.whitespace + string.punctuation + string.digits + '”“‘’'
+    return word.strip(chars)
+
+
+def process_line(line: str, words: list):
+    """Splits a line into words and counts the number of words."""
+
+    line = line.replace('—', ' ')
+    for word in line.split():
+        pw = process_word(word)
+        words.append(pw)
+
+
+def word_list(filename):
+    """Makes a list of the words from a file."""
+    a = []
+    with open(filename, encoding='utf-8') as fin:
+        skip_header(fin)
+        for line in fin:
+            if line.startswith('End of the Project Gutenberg'):
+                break
+            process_line(line, a)
+    return a
+
+
+def markov_analysis(filename: str, n=2):
+    """Performs Markov analysis.
+
+    :param: n: number of words in the prefix
+    """
+    d = {}
+
+    words = word_list(filename)
+    total = len(words)
+    for i in range(total):
+        if total - i == n:
+            break
+
+        prefix = tuple(words[i:i+n])
+        suffix = words[i+n]
+        d[prefix] = d.get(prefix, []) + [suffix]
+
+    return d
+
+
+def random_text(suffix_map: dict, n=100):
+    """Generates random words from the analyzed text.
+
+    Starts with a random prefix from the dictionary.
+
+    :param: n: number of words to generate
+    """
+
+    # Start prefix
+    prefix = random.choice(list(suffix_map.keys()))
+
+    for i in range(n):
+        suffixes = suffix_map[prefix]
+
+        word = random.choice(suffixes)
+        print(word, end=' ')
+
+        # Next prefix
+        prefix = prefix[1:] + (word,)
+
+
+def main():
+    m = markov_analysis('war_and_peace.txt', 2)
+    random_text(m, 20)
+
+
+if __name__ == '__main__':
+    main()
